955f50e4587b4502c22b3e98abe6b9f0c0253d29
Andrea Wu <lixianhyun@gmail.com>
2020-11-08 22:18:35 -0800

    displaying max 10 cities on search 'input' and addEventListener to cities by clicking selected city and display it then

--- main.js
+++ main.js
@@ -1,22 +1,66 @@
-const express = require("express"),
-    app     = express(),
-    request = require("request");
+let data;
 
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public")); 
+window.onload = function() {
 
-app.get("/", function(req, res){
-    
-    const url = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+    let request = new XMLHttpRequest();
+        request.open("GET", "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json");
+        request.send();
+        request.onload = () => {
+            if(request.status === 200) {
+                data = JSON.parse(request.response); // **refering 
+                // data = [...JSON.parse(request.response)] //**copying */
+            } else {
+                console.log(`error ${request.status} ${request.statusText}`);
+            }
+        }
+}
 
-    request(url, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            const data = JSON.parse(body);
-            res.render("landing", {data});
-        };
-    });
-});
+function getValueEl(value) {
+    const keyword = value.trim();
+    const ulList = document.getElementById("ul");
+    const cityInfo = document.getElementById("cityInfo");
+    ulList.innerHTML = '';
+    cityInfo.innerHTML = '';
+    let count = 0;
+        
+    if(keyword !== ""){
+        const regex = new RegExp(`^${value}`, 'i');
+        const srcCities = data.filter((item) => regex.test(item.city));
+        // console.log(srcCities);
 
-app.listen(3000, function(){
-    console.log("Server Running!")
-})
+        for (let obj of srcCities) {
+            if(count < 15) {
+                const liElement = document.createElement("li");
+                const spanElement = document.createElement("span");
+                liElement.textContent = obj.city + ", " + obj.state;
+                spanElement.textContent = obj.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                ulList.appendChild(liElement);
+                liElement.appendChild(spanElement);
+                count++;
+
+                liElement.addEventListener("click", function(){
+                    const input = document.getElementById("input");
+                    const cityName = document.createElement("p");
+                    const population = document.createElement("p");
+                    const pplGrow = document.createElement("p");
+
+                    input.value = '';
+                    ulList.innerHTML = '';
+
+                    cityName.textContent = obj.city.toUpperCase() + ", " + obj.state;
+                    population.textContent = obj.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    pplGrow.textContent = obj.growth_from_2000_to_2013; 
+
+                    cityName.classList.add("fas", "fa-map-marker-alt");
+                    population.classList.add("fas", "fa-users");
+                    pplGrow.classList.add("fas", "fa-chart-line")
+                    cityInfo.appendChild(cityName);
+                    cityInfo.appendChild(population);
+                    cityInfo.appendChild(pplGrow);
+                })
+            } else {
+                break;
+            }
+        }
+    }
+}
